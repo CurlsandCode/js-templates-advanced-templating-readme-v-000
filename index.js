4c08182d362ccc5a697989ba656e3976f8f040f6
Monika Williams <monika.m.williams@gmail.com>
2017-12-17 06:38:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function loadIssue() {
+function loadIssues() {
   var issue = {
     state: "closed",
     number: 5,
@@ -9,5 +9,5 @@ function loadIssue() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
    var result = template(issues);
     document.getElementsByTagName("main")[0].innerHTML += result;
-  
+
 }
